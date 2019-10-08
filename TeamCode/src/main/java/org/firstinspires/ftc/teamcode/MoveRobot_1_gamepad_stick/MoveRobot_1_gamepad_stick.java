4f46b2604a680e6895b1aa327b50e1e18a0c612a
Andy3153 <andy3153@protonmail.com>
2019-10-08 13:16:58 +0300

    Modified the program because of some wires

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveRobot_1_gamepad_stick/MoveRobot_1_gamepad_stick.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveRobot_1_gamepad_stick/MoveRobot_1_gamepad_stick.java
@@ -5,25 +5,25 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
-public class MoveRobot_1_gamepad_stick extends LinearOpMode {
+public class MoveRobot_1_gamepad_stick extends LinearOpMode
+{
+
     @Override
-    public void runOpMode() throws InterruptedException {
-
-
+    public void runOpMode() throws InterruptedException
+    {
         //Declara motoarele
         DcMotor forMotor1 = hardwareMap.get(DcMotor.class, "motor1");
         DcMotor forMotor2 = hardwareMap.get(DcMotor.class, "motor2");
 
-        while (true) {
-
-            //Seteaza curentul la 0
+        while (true)
+        {
+            //Seteaza curentul la 0 cand nu face nimic
             forMotor1.setPower(0);
             forMotor2.setPower(0);
 
             //Pt miscarea fata-spate
-            forMotor1.setPower(gamepad1.left_stick_y);
+            forMotor1.setPower(-gamepad1.left_stick_y);
             forMotor2.setPower(gamepad1.left_stick_y);
-
 
             if (gamepad1.left_stick_x != 0)
             {
@@ -31,25 +31,24 @@ public class MoveRobot_1_gamepad_stick extends LinearOpMode {
                 if (gamepad1.left_stick_x < 0 )
                 {
                     forMotor1.setPower(gamepad1.left_stick_x);
-                    forMotor2.setPower(-gamepad1.left_stick_x);
+                    forMotor2.setPower(gamepad1.left_stick_x);
                 }
 
                 //Pt miscarea pe diagonala pe dreapta
                 if (gamepad1.left_stick_x > 0 )
                 {
                     forMotor1.setPower(gamepad1.left_stick_x);
-                    forMotor2.setPower(-gamepad1.left_stick_x);
+                    forMotor2.setPower(gamepad1.left_stick_x);
                 }
-
-
             }
 
 
 
 
-
-
-
         }
+
+
+
+
     }
 }
